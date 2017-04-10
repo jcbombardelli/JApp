413234ced33e4d136af3261750cb9f7d1f454189
JC Bombardelli <jc.bombardelli@live.com>
2017-04-10 07:46:34 -0300

    Init

---
+++ JApp/JApp/Login.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace JApp
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void nav(Object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MainPage();
+            //Navigation.PushAsync(new NavigationPage(new MainPage())); 
+        }
+    }
+}
